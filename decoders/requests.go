2678e1d46e251f9dea39c5cdc44c1270400a0d40
Chance Graff <cgraffrun@gmail.com>
2020-05-01 11:06:22 -0500

    Pass interface by value

--- decoders/requests.go
+++ decoders/requests.go
@@ -6,7 +6,7 @@ import (
 )
 
 // RequestBody returns the body from a request
-func RequestBody(i *interface{}, rq *http.Request) error {
+func RequestBody(i interface{}, rq *http.Request) error {
 	err := json.NewDecoder(rq.Body).Decode(&i)
 	if err != nil {
 		return err
