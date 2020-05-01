843521cfb9e8a84b93ae1484ec0141293e07af08
Chance Graff <cgraffrun@gmail.com>
2020-05-01 13:24:01 -0500

    Add http logger

---
+++ loggers/http.go
@@ -0,0 +1,41 @@
+package golog
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HTTPLogger logs interactions
+type HTTPLogger struct {
+	name string
+	wt   *http.ResponseWriter
+	je   *json.Encoder
+}
+
+// NewHTTPLogger creates a new logger
+func NewHTTPLogger(name string, wt *http.ResponseWriter) HTTPLogger {
+	(*wt).Header().Set("Content-Type", "text/html; charset=utf-8")
+	(*wt).Header().Set("Access-Control-Allow-Origin", "*")
+
+	return HTTPLogger{
+		name: name,
+		wt:   wt,
+		je:   json.NewEncoder(*wt),
+	}
+}
+
+// Okay records success
+func (l *HTTPLogger) Okay(v interface{}) {
+	log.Printf("%s::Success\r\n", l.name)
+	(*l.wt).WriteHeader(http.StatusOK)
+	l.je.Encode(v)
+}
+
+// Error records malfunctions
+func (l *HTTPLogger) Error(err error, status int) {
+	log.Printf("%s::Error:::%s, %s\r\n", l.name, fmt.Sprint(status), err)
+	(*l.wt).WriteHeader(status)
+	l.je.Encode(map[string]string{"status": "error"})
+}
